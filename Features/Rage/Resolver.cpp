95e743150a07ffe747ea57a2a6093f4155e25974
rxvan <rxvan@rxvan.wtf>
2021-09-19 01:51:11 +0100

    old resolver hit mad pp

--- Features/Rage/Resolver.cpp
+++ Features/Rage/Resolver.cpp
@@ -1,4 +1,3 @@
-
 #include "../../Hooks/hooks.h"
 #include "../Features.h"
 
@@ -106,57 +105,23 @@ bool shot(IBasePlayer* p)
 	return false;
 }
 
-//void CResolver::Resolver(IBasePlayer* p)
-//{
-//	int i = p->EntIndex();
-//	CCSGOPlayerAnimState* state = p->GetPlayerAnimState();
-//
-//	if (!vars.ragebot.resolver || p->GetPlayerInfo().fakeplayer)
-//	{
-//		ResolverMode[i] = p->GetPlayerInfo().fakeplayer ? str("Bot") : str("Disabled");
-//		ResolverInfo[i].Index = 0;
-//		return;
-//	}
-//
-//	int add = 0;
-//	if (!shot_snapshots.empty()) {
-//		const auto& snapshot = shot_snapshots.front();
-//		const bool& dt_ready = !csgo->need_to_recharge && g_Misc->dt_bullets <= 1 && csgo->weapon->isAutoSniper();
-//		if (dt_ready && snapshot.hitbox == 0 && snapshot.intended_damage > p->GetHealth()) {
-//			add = g_Misc->dt_bullets;
-//		}
-//	}
-//
-//	bool max_desync[64] = { false };
-//
-//	int missed_shots = (csgo->actual_misses[i] + csgo->imaginary_misses[i] + add) % 4;
-//
-//	
-//}
-
-bool CResolver::Do(IBasePlayer* p) {
+void CResolver::Resolver(IBasePlayer* p)
+{
 	int i = p->EntIndex();
-	int side = FreestandSide[i];
 	CCSGOPlayerAnimState* state = p->GetPlayerAnimState();
-	ResolverMode[i] = "";
-	if (!vars.ragebot.resolver)
-	{
-		ResolverMode[i] += "Disabled";
-		ResolverInfo[i].Index = 0;
-		return false;
-	}
 
 	if (p->GetPlayerInfo().fakeplayer)
 	{
-		ResolverMode[i] += "Bot";
+		ResolverMode[i] = str("Bot");
 		ResolverInfo[i].Index = 0;
-		return false;
+		return;
 	}
 
-	if (shot(p)) {
-		ResolverMode[i] += "Onshot";
-		record.LastKnownYaw[i] = state->m_abs_yaw;
-		ResolverInfo[i].Index = i;
+	if (!vars.ragebot.resolver)
+	{
+		ResolverMode[i] = str("Disabled");
+		ResolverInfo[i].Index = 0;
+		return;
 	}
 
 	int add = 0;
@@ -171,79 +136,292 @@ bool CResolver::Do(IBasePlayer* p) {
 	bool max_desync[64] = { false };
 
 	int missed_shots = (csgo->actual_misses[i] + csgo->imaginary_misses[i] + add) % 4;
-	float time_since_0_pitch[64];
 
-	while (p->GetEyeAngles().x == 0.f)
+	float last_yaw = ResolverInfo[i].ResolvedAngle;
+	float angle = ResolverInfo[i].ResolvedAngle;
+	int side = FreestandSide[i];
+	string mode = ResolverMode[i];
+	int index = ResolverInfo[i].Index;
+
+	if (shot(p))
+		csgo->last_shoot_time[i] == p->GetSimulationTime();
+
+	if (csgo->last_shoot_time[i] == p->GetSimulationTime() || shot(p))
 	{
-		time_since_0_pitch[i] = csgo->curtime;
+		mode = "Onshot";
+		angle = last_yaw;
+		index = i;
+		return;
 	}
 
-	// bool HighDeltaDesync[64] = { p->GetSequence() == 979 && p->GetFlags() & FL_ONGROUND && p->GetVelocity().Length2D() <= 0.1f };
-
-	if (missed_shots <= 2)
+	if (missed_shots < 2)
 	{
-		/*if (HighDeltaDesync)
+		if (p->GetSequence() == 979 && p->GetVelocity().Length2D() < 0.1f)
+			max_desync[i] = true;
+
+		if (side != 0)
 		{
-			ResolverMode[i] += "High Delta";
-			ResolverInfo[i].Index = i;
-			ResolverInfo[i].ResolvedAngle -= record.LastKnownYaw[i] + 58.f * side;
-			record.IsExtending[i] = true;
-			record.IsSwaying[i] = false;
-		}
-		else*/
-		{
-			if (record.LastKnownYaw[i] != state->m_abs_yaw)
+			mode = "Resolved ";
+			if (max_desync[i])
 			{
-				// sway possibly?
-				float lby_timer[64];
-				if (p->GetVelocity().Length2D() > 0.1f)
-					lby_timer[i] = csgo->curtime + .22f;
-				if (!(p->GetFlags() & FL_ONGROUND))
+				angle = 60.f * side;
+				mode += "max desync";
+				index = i;
+			}
+			else
+			{
+				auto cur_abs_yaw = g_Animfix->IS_Animstate[i]->m_abs_yaw;
+				if (cur_abs_yaw != last_yaw)
 				{
-					ResolverMode[i] += "In air";
-					ResolverInfo[i].Index = i;
-					ResolverInfo[i].ResolvedAngle = record.LastKnownYaw[i];
-					record.IsExtending[i] = false;
-					record.IsSwaying[i] = false;
+					mode += "sway";
+					angle = cur_abs_yaw - last_yaw;
+					index = i;
 				}
 				else
 				{
-					if (lby_timer[i] == csgo->curtime) // they can break lby rn
+					if (cur_abs_yaw == last_yaw && p->GetVelocity().Length2D() < 0.1f)
 					{
-						ResolverInfo[i].Index = i;
-						ResolverInfo[i].ResolvedAngle = record.LastKnownYaw[i] + (58.f /*max*/ * side /*freestand side*/ / 2.4f /*sway timer*/);
-						record.IsSwaying[i] = true;
-						record.IsExtending[i] = false;
-						ResolverMode[i] += "LBY";
-					}
-					else // they cant break lby, so we'll just go for low delta?
-					{
-						ResolverInfo[i].Index = i;
-						ResolverInfo[i].ResolvedAngle = record.LastKnownYaw[i] + (58.f * side - 28.f);
-						record.IsExtending[i] = false;
-						record.IsSwaying[i] = false;
-						ResolverMode[i] += "Low delta";
+						// possible lby breaking low delta (scary shit);
+
+						mode = "low delta";
+						angle = cur_abs_yaw + 30.f * side;
+						index = i;
+
 					}
 				}
 			}
 		}
-		record.LastKnownYaw[i] = state->m_abs_yaw;
-	}
-	else // lets just run a simple bruteforce, dont need anything special here;
-	{
-		switch (missed_shots % 5)
+		else
 		{
+			if (last_yaw != 0)
+			{
+				mode = "Bruteforce shot ";
+				switch (missed_shots % 2)
+				{
+				case 0:
+					angle = last_yaw + 10.f * side;
+					mode += "1";
+					index = i;
+					break;
+				case 1:
+					angle = last_yaw + 25.f * side;
+					mode += "2";
+					index = i;
+					break;
+				}
+			}
+			else
+				mode = "Unresolved";
+		}
+	}
+	else
+	{
+		mode = "Bruteforce shot ";
+		switch (missed_shots % 5) // yes i know... 5??? first 2 are skipped because of the unresolved shit from above^
+		{
+		case 2:
+			mode += "3";
+			angle = angle + 25.f * side;
+			index = i;
+			break;
 		case 3:
-			ResolverInfo[i].Index = i;
-			ResolverInfo[i].ResolvedAngle = record.LastKnownYaw[i] * side;
-			ResolverMode[i] += "Bruteforce shot 1";
+			mode += "4";
+			angle = angle + 15.f * side;
+			index = i;
 			break;
 		case 4:
-			ResolverInfo[i].Index = i;
-			ResolverInfo[i].ResolvedAngle = record.LastKnownYaw[i] * side - 15.f;
-			ResolverMode[i] += "Bruteforce shot 2";
+			mode += "5";
+			angle = angle + 10.f * side;
+			index = i;
 			break;
 		}
 	}
-	return true;
+}
+
+bool CResolver::Do(IBasePlayer* player) {
+	if (player->GetPlayerInfo().fakeplayer)
+		return false;
+	else
+	{
+		Resolver(player);
+		return true;
+	}
+
+	//	static int offresolver_ticks[65] = { 0 };
+	//	static float last_velocity[65] = { 0.f };
+	//	static int ticks_with_zero_pitch[65] = { 0.f };
+	//
+	//	int idx = player->GetIndex();
+	//	auto animstate = player->GetPlayerAnimState();
+	//	if (!animstate || !vars.ragebot.enable || player->HasGunGameImmunity() || !vars.ragebot.resolver || !csgo->local->isAlive()) {
+	//		ResolverMode[idx] = str("OFF");
+	//		ResolverInfo[idx].Index = 0;
+	//		return false;
+	//	}
+	//
+	//	if (player->GetEyeAngles().x < 45.f)
+	//		ticks_with_zero_pitch[idx]++;
+	//	else
+	//		ticks_with_zero_pitch[idx] = 0;
+	//
+	//	int add = 0;
+	//	if (!shot_snapshots.empty()) {
+	//		const auto& snapshot = shot_snapshots.front();
+	//		const bool& dt_ready = !csgo->need_to_recharge && g_Misc->dt_bullets <= 1 && csgo->weapon->isAutoSniper();
+	//		if (dt_ready && snapshot.hitbox == 0 && snapshot.intended_damage > player->GetHealth()) {
+	//			add = g_Misc->dt_bullets;
+	//		}
+	//	}
+	//
+	//	int missed_shots = (csgo->actual_misses[idx] + csgo->imaginary_misses[idx] + add) % 4;
+	//
+	//#ifndef _DEBUG
+	//	if (player->GetPlayerInfo().fakeplayer) {
+	//		ResolverMode[idx] = str("OFF");
+	//		ResolverInfo[idx].Index = 0;
+	//		return false;
+	//	}
+	//#endif
+	//
+	//	const float delta = 60.f;
+	//	const float angle = GetAngle(player);
+	//
+	//	const float low_delta = player->GetDSYDelta() * 0.5f;
+	//
+	//	float desync_angle = delta;
+	//
+	//	const bool& sideways =
+	//		fabsf(Math::NormalizeYaw(angle - GetLeftYaw(player))) < 45.f
+	//		||
+	//		fabsf(Math::NormalizeYaw(angle - GetRightYaw(player))) < 45.f;
+	//
+	//	const bool& forward = fabsf(Math::NormalizeYaw(angle - GetForwardYaw(player))) < 90.f && !sideways;
+	//
+	//	if (!HitInfo[idx].empty()) {
+	//		const auto& info = HitInfo[idx].front();
+	//		if ((csgo->actual_misses[idx] + csgo->imaginary_misses[idx]) <= 0) {
+	//			static std::string AA = str("AA");
+	//			static std::string AB = str("AB");
+	//			static std::string AC = str("AC");
+	//
+	//			int side = FreestandSide[idx];
+	//			if (side != 0)
+	//			{
+	//				if (info.Stage == inverse_antifreestand) {
+	//					side *= -1;
+	//					ResolverMode[idx] = AA;
+	//					ResolverInfo[idx].Stage = EResolverStages::inverse_antifreestand;
+	//				}
+	//				else {
+	//					ResolverInfo[idx].Stage = EResolverStages::antifreestand;
+	//					ResolverMode[idx] = AB;
+	//				}
+	//
+	//				ResolverInfo[idx].Index = forward ? -side : side;
+	//			}
+	//			else {
+	//				ResolverInfo[idx].Index = 0;
+	//				ResolverMode[idx] = AC;
+	//			}
+	//			if (info.LowDelta) {
+	//				desync_angle = low_delta;
+	//				ResolverMode[idx].insert(ResolverMode[idx].begin(), 'l');
+	//				ResolverInfo[idx].LowDelta = true;
+	//			}
+	//			else {
+	//				ResolverInfo[idx].LowDelta = false;
+	//				ResolverMode[idx].insert(ResolverMode[idx].begin(), 'd');
+	//			}
+	//			return false;
+	//		}
+	//	}
+	//
+	//	if (missed_shots >= 2) {
+	//		const float& velocity = player->GetVelocity().Length2D();
+	//		const bool& additional_check = player->GetDuckAmount() >= 0.8f || player->GetEyeAngles().x <= 60.f;
+	//		if (Math::IsNearEqual(velocity, last_velocity[idx], 10.f)) {
+	//			if (velocity <= 120.f) {
+	//				if (!additional_check)
+	//					desync_angle = low_delta;
+	//				else {
+	//					if (missed_shots >= 3 && additional_check)
+	//						desync_angle = low_delta;
+	//				}
+	//			}
+	//		}
+	//	}
+	//
+	//	if (missed_shots == 0) {
+	//		if (FreestandSide[idx] != 0) {
+	//			ResolverMode[idx] = hs::C.s();
+	//
+	//			if (forward) {
+	//				ResolverMode[idx] += hs::B.s();
+	//			}
+	//			else
+	//				ResolverMode[idx] += hs::A.s();
+	//			ResolverInfo[idx].Index = forward ? -FreestandSide[idx] : FreestandSide[idx];
+	//			ResolverInfo[idx].Stage = EResolverStages::antifreestand;
+	//		}
+	//		else
+	//		{
+	//			static std::string CC = str("CC");
+	//			ResolverMode[idx] = CC;
+	//			ResolverInfo[idx].Index = 0;
+	//			ResolverInfo[idx].Stage = EResolverStages::unresolved;
+	//		}
+	//	}
+	//	else {
+	//		switch (missed_shots % 2)
+	//		{
+	//		case 0:
+	//			if (FreestandSide[idx] == 0)
+	//				FreestandSide[idx] = 1;
+	//			break;
+	//		case 1:
+	//			if (FreestandSide[idx] == 0)
+	//				FreestandSide[idx] = -1;
+	//		}
+	//
+	//		if (forward) {
+	//			switch (missed_shots % 2) {
+	//			case 0:
+	//				ResolverMode[idx] = hs::DB.s();
+	//				ResolverInfo[idx].Index = -FreestandSide[idx];
+	//				break;
+	//			case 1:
+	//				ResolverMode[idx] = hs::DA.s();
+	//				ResolverInfo[idx].Index = FreestandSide[idx];
+	//				break;
+	//			}
+	//		}
+	//		else {
+	//			switch (missed_shots % 2) {
+	//			case 0:
+	//				ResolverMode[idx] = hs::DB.s();
+	//				ResolverInfo[idx].Index = FreestandSide[idx];
+	//				break;
+	//			case 1:
+	//				ResolverMode[idx] = hs::DA.s();
+	//				ResolverInfo[idx].Index = -FreestandSide[idx];
+	//				break;
+	//			}
+	//		}
+	//		if (desync_angle != low_delta)
+	//			ResolverInfo[idx].Stage = EResolverStages::inverse_antifreestand;
+	//	}
+	//
+	//	if (desync_angle == low_delta) {
+	//		ResolverMode[idx].insert(ResolverMode[idx].begin(), 'l');
+	//		ResolverInfo[idx].LowDelta = true;
+	//	}
+	//	else {
+	//		ResolverInfo[idx].LowDelta = false;
+	//		ResolverMode[idx].insert(ResolverMode[idx].begin(), 'd');
+	//	}
+	//	last_velocity[idx] = player->GetVelocity().Length2D();
+	//
+	//	if (ResolverInfo[idx].Index != 0)
+	//		ResolverInfo[idx].ResolvedAngle = Math::NormalizeYaw(angle + (desync_angle * ResolverInfo[idx].Index));
+	//	return true;
 }
